3661d88a38293086df56e95b1e03440a397e3bf1
Savin Osuka <osukasavin2001@gmail.com>
2022-09-30 14:48:40 +0300

    Firebase communication successful for registration

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { auth } from "./assets/firebase/firebase";
+import {AuthProvider} from "./assets/firebase/AuthContext"
+import { onAuthStateChanged } from "firebase/auth";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Login from "./pages/auth-pages/Login";
@@ -8,25 +11,34 @@ import Contact from "./pages/home-pages/Contact";
 import Home from "./pages/home-pages/Home";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+    });
+  }, []);
   return (
     <div className="App">
       <Router>
-        <Navbar />
-        <Routes>
-          {/* Landing pages */}
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
+        <AuthProvider value={{ currentUser }}>
+          <Navbar />
+          <Routes>
+            {/* Landing pages */}
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
 
-          {/* Authentication pages */}
-          <Route path="sign-up" element={<Register />} />
-          <Route path="sign-in" element={<Login />} />
+            {/* Authentication pages */}
+            <Route path="sign-up" element={<Register />} />
+            <Route path="sign-in" element={<Login />} />
 
-          {/* Admin pages */}
-          
-          {/* Writer pages */}
-          {/*  */}
-        </Routes>
+            {/* Admin pages */}
+
+            {/* Writer pages */}
+            {/*  */}
+          </Routes>
+        </AuthProvider>
       </Router>
     </div>
   );
--- src/assets/firebase/firebase.js
+++ src/assets/firebase/firebase.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 import {getAuth} from 'firebase/auth'
 
@@ -17,5 +16,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app)
+
+export {auth}
 
