71f3c00d9e3683dc2288616ca534d6a17ea0507d
Savin Osuka <osukasavin2001@gmail.com>
2022-10-17 13:20:40 +0300

    File upload working

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -1,14 +1,41 @@
-import React from "react";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import React, { useState } from "react";
 import { FaFileUpload } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { useAuthValue } from "../assets/firebase/AuthContext";
 import { store } from "../assets/firebase/firebase";
 
 const FileUpload = () => {
+  const { currentUser } = useAuthValue();
+  const [error, setError] = useState(null);
+  const [docPath, setDocPath] = useState(null);
+  const [progress, setProgress] = useState(null);
   const fileHandler = (e) => {
     e && e.preventDefault();
     const file = e.target[0].files[0];
-    console.log(file.name);
+    uploadFile(file);
   };
-  const uploadFile = (file) => {};
+  const uploadFile = (file) => {
+    if (!file) {
+      return setError("Kindly choose a file for upload");
+    }
+    const storageRef = ref(store, `/files/${currentUser.uid}/${file.name}`);
+    const uploadDoc = uploadBytesResumable(storageRef, file);
+    uploadDoc.on(
+      "state_changed",
+      (snapshot) => {
+        const uploadProgress =
+          Math.round(snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setProgress(uploadProgress);
+      },
+      (err) => {
+        setError(err.message);
+      },
+      () => {
+        getDownloadURL(uploadDoc.snapshot.ref).then((url) => setDocPath(url));
+      }
+    );
+  };
   return (
     <div className="container mx-auto">
       <div className="w-full flex items-center justify-center py-12 lg:px-8">
@@ -35,51 +62,91 @@ const FileUpload = () => {
             className="modal-toggle"
           />
           <div className="modal">
-            <form className="modal-box" onSubmit={fileHandler}>
-              <div>
-                <label className="block text-md font-semibold text-neutral">
-                  File upload
-                </label>
-                <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-                  <div className="space-y-1 text-center">
-                    <FaFileUpload className="mx-auto h-12 w-12 text-accent font-thin" />
-
-                    <div className="flex text-sm text-gray-600">
-                      <label
-                        htmlFor="file-upload"
-                        className="relative cursor-pointer bg-base-100 rounded-md font-medium text-primary hover:underline"
-                      >
-                        <span>Upload a file</span>
-                        <input
-                          id="file-upload"
-                          name="file-upload"
-                          type="file"
-                          className="sr-only"
-                        />
-                      </label>
-                      <p className="pl-1">or drag and drop</p>
+            {progress ? (
+              <>
+                <div className="modal-box flex flex-col items-center justify-around">
+                  <h3 className="block text-md font-semibold text-neutral mb-9">
+                    File uploading
+                  </h3>
+                  <div className="flex justify-center items-center">
+                    <div
+                      className="radial-progress text-primary"
+                      style={{ "--value": progress }}
+                    >
+                      {progress} %
                     </div>
-                    <p className="text-xs text-accent">
-                      DOC, DOCX, XLS, XLXS, PNG, JPG and PDF up to 10MB
-                    </p>
                   </div>
+                  {/* {docPath && (
+                    <p className="underline">File path: {docPath.slice(0, 30)}...</p>
+                  )} */}
+                  {progress === 100 && (
+                    <div className="modal-action  uppercase">
+                      <Link to={"/"}>
+                        <label
+                          htmlFor="profile-pic-modal"
+                          className="btn btn-sm btn-outline btn-error rounded-2xl"
+                        >
+                          close
+                        </label>
+                      </Link>
+                    </div>
+                  )}
                 </div>
-              </div>
-              <div className="modal-action flex justify-between uppercase">
-                <label
-                  htmlFor="profile-pic-modal"
-                  className="btn btn-sm btn-outline btn-error rounded-2xl"
-                >
-                  cancel
+              </>
+            ) : (
+              <>
+                <form className="modal-box" onSubmit={fileHandler}>
+                  <div>
+                    <label className="block text-md font-semibold text-neutral">
+                      File upload
+                    </label>
+                    <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+                      <div className="space-y-1 text-center">
+                        <FaFileUpload className="mx-auto h-12 w-12 text-accent font-thin" />
+
+                        <div className="flex text-sm text-gray-600">
+                          <label
+                            htmlFor="file-upload"
+                            className="relative cursor-pointer bg-base-100 rounded-md font-medium text-primary hover:underline"
+                          >
+                            <input
+                              id="file-upload"
+                              name="file-upload"
+                              type="file"
+                              className="file:btn file:btn-sm file:btn-primary file:rounded-2xl"
+                            />
+                          </label>
+                        </div>
+                        <p className="text-xs text-accent">
+                          DOC, DOCX, XLS, XLSX, PNG, JPG and PDF up to 10MB
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="modal-action flex justify-between uppercase">
+                    <label
+                      htmlFor="profile-pic-modal"
+                      className="btn btn-sm btn-outline btn-error rounded-2xl"
+                    >
+                      cancel
+                    </label>
+                    <button
+                      type="submit"
+                      className="btn btn-sm btn-primary rounded-2xl"
+                    >
+                      upload file
+                    </button>
+                  </div>
+                </form>
+              </>
+            )}
+            {error && (
+              <div className=" text-sm uppercase p-4 text-base-100 bg-error text-center rounded-3xl">
+                <label htmlFor="profile-pic-modal" className="mt-2">
+                  {error}
                 </label>
-                <button
-                  type="submit"
-                  className="btn btn-sm btn-primary rounded-2xl"
-                >
-                  upload file
-                </button>
               </div>
-            </form>
+            )}
           </div>
         </>
       </div>
